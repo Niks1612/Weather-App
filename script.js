ca642fb6aed0772227cf0284d0d241ce548931eb
Nikita Phartyal <133484285+Niks1612@users.noreply.github.com>
2024-01-01 17:44:40 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+const apiKey = 'b99bf3bdf58978240916330ae3c02da8';
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
+
+const locationInput = document.getElementById('locationInput');
+const searchButton = document.getElementById('searchButton');
+const locationElement = document.getElementById('location');
+const temperatureElement = document.getElementById('temperature');
+const descriptionElement = document.getElementById('description');
+const humidityElement = document.getElementById('humidity');
+const feelsLikeElement = document.getElementById('feelsLike');
+
+searchButton.addEventListener('click', () => {
+    const location = locationInput.value;
+    if (location) {
+        fetchWeather(location);
+    }
+});
+
+function fetchWeather(location) {
+    const url = `${apiUrl}?q=${location}&appid=${apiKey}&units=metric`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            locationElement.textContent = data.name;
+            temperatureElement.textContent = `${Math.round(data.main.temp)}°C`;
+            descriptionElement.textContent = data.weather[0].description;
+            humidityElement.textContent = `Humidity: ${data.main.humidity}% `;
+            feelsLikeElement.textContent = `Feels Like: ${Math.round(data.main.feels_like)}°C`;
+        })
+        .catch(error => {
+            console.error('Error fetching weather data:', error);
+        });
+}
